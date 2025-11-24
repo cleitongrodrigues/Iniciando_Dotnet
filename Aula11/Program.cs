e32f6a3b4edf2d8340f8307d29dbec69968d3e7e
Cleiton Guilherme Rodrigues de Souza <159494633+cleitongrodrigues@users.noreply.github.com>
2025-11-23 21:27:18 -0300

    Criando checagem de idade para votação

---
+++ Aula11/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula11;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite sua idade:");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        switch(idade)
+        {
+            case < 16:
+                Console.WriteLine("Não pode votar");
+                break;
+            case >= 16 and < 18 or >= 65:
+                Console.WriteLine("Voto facultativo");
+                break;
+            default:
+                Console.WriteLine("Voto obrigatório!");
+                break;
+        }
+    }
+}
